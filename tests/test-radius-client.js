ccf0b8e9330c1cf90690a03506e6f65c5376896b
Chris Walker <chris@thechriswalker.net>
2011-06-29 21:42:35 +0100

    Updating testing code

--- tests/test-radius-client.js
+++ tests/test-radius-client.js
@@ -3,11 +3,26 @@ var Radius = require("../lib/RADIUS.js");
 
 var radius_client = new Radius.Client();
 
+/*
+ 	Add this to your /etc/freeradius/users (or /etc/raddb/users if on RHEL)
+	Also ensure you load the Nomadix Dictionaryin both server and client config
+
+testuser        Cleartext-Password := "testpass"
+                Service-Type = Framed-User,
+                Reply-Message = "No VSAs"
+
+testuser2       Cleartext-Password := "testpass"
+                Service-Type = Framed-User,
+                Reply-Message = "Nomadix VSA",
+                Nomadix-Bw-Down = 1024,
+                Nomadix-Bw-Up = 512
+*/
+
 var user_vsa = {"user-name": "testuser2", "password": "testpass", "service-type": "framed-user"};
 var user_no_vsa = {"user-name": "testuser", "password": "testpass", "service-type": "framed-user"};
 
-var expected_vsa = [0, [["Service-Type", "Framed-User"], ["Reply-Message", "Nomadix VSA Attribute"], ["Nomadix-Bw-Down", "1024"]] ];
-var expected_no_vsa = [0, [["Service-Type", "Framed-User"], ["Reply-Message", "No VSA Attributes"]] ];
+var expected_vsa = [0, {"Service-Type": ["Framed-User"], "Reply-Message": ["Nomadix VSA"], "Nomadix-Bw-Down": ["1024"], "Nomadix-Bw-Up":["512"]} ];
+var expected_no_vsa = [0, {"Service-Type": ["Framed-User"], "Reply-Message": ["No VSAs"]} ];
 
 var tests = [
     { name: "Request expects no VSA in Response",
